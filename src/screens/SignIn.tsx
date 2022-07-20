4b25ce6fdf44b30211ecfa0f4c78bc064fb0d7bf
vinicius <nascimento.vinicius32@gmail.com>
2022-07-20 14:45:16 -0300

    include button components

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,22 @@
 import { Heading, Icon, VStack, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
+import { useState } from "react";
 
 import Logo from "../assets/logo_primary.svg";
+
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   const { colors } = useTheme();
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        Acesse sua conta
+        Acesse sua conta {name}
       </Heading>
 
       <Input
@@ -19,12 +25,17 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setName}
       />
       <Input
+        mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
+
+      <Button title="Entrar" w="full" />
     </VStack>
   );
 }
